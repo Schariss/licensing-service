05c77d331dd93553fd702833fd9b9bc95b9f0ef4
Chahid Adnane <adnanechahid@gmail.com>
2022-08-08 05:22:28 +0200

    feat(test): implement nested tests and handle license not found case

--- src/test/java/com/optimagrowth/license/controller/LicenseControllerTest.java
+++ src/test/java/com/optimagrowth/license/controller/LicenseControllerTest.java
@@ -1,21 +1,18 @@
 package com.optimagrowth.license.controller;
 
+import com.optimagrowth.license.exception.ResourceNotFoundException;
 import com.optimagrowth.license.model.License;
 import com.optimagrowth.license.service.LicenseService;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -24,40 +21,80 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(LicenseController.class)
 class LicenseControllerTest {
 
+    private LicenseRequestBuilder requestBuilder;
     @MockBean
     private LicenseService licenseService;
     @Autowired
     private MockMvc mockMvc;
-    private static String baseUrl;
+    private static String BASEURL;
 
     @BeforeAll
-    static void beforeAll() {
-        baseUrl = "/v1/organization/{organizationId}/license";
+    static void setBaseUrl() {
+        BASEURL = "/v1/organization/{organizationId}/license";
+    }
+
+    @BeforeEach
+    void configureSystemUnderTest() {
+        requestBuilder = new LicenseRequestBuilder(mockMvc);
     }
 
     @Nested
-    @DisplayName("getLicence")
-    class GetLicence{
-        @Test
-        @DisplayName("When licenseId and OrganizationId found")
-        void getLicense() throws Exception {
-            String organizationId = "OptimaGrowth";
-            String licenseId = "1234";
-            License license = new License(licenseId, "description", organizationId, "productName",
-                    "licenseType", "I AM DEFAULT");
-            when(licenseService.getLicense(licenseId, organizationId)).thenReturn(license);
-            mockMvc.perform(get(baseUrl + "/{licenseId}", organizationId, licenseId))
-                    .andExpect(status().isOk())
-                    .andExpect(content().contentType("application/hal+json"))
-                    .andExpect(jsonPath("$.licenseId", is(licenseId)))
-                    .andExpect(jsonPath("$.organizationId", is(organizationId)))
-                    .andDo(print());
+    @DisplayName("find licence by license id and organization id")
+    class GetLicenceByLicenseIdAndOrganizationId{
+        private static final String LICENSE_ID = "12345678";
+        private static final String ORGANIZATION_ID = "OptimaGrowth";
+        @Nested
+        @DisplayName("When the requested license is not found")
+        class WhenRequestedLicenseIsNotFound{
+            @BeforeEach
+            void throwException() {
+                when(licenseService.getLicense(LICENSE_ID, ORGANIZATION_ID))
+                        .thenThrow(new ResourceNotFoundException(
+                                "Unable to find license with License id"));
+            }
+
+            @Test
+            @DisplayName("Should return the HTTP status code not found (404)")
+            void shouldReturnHttpStatusCodeNotFound() throws Exception {
+                requestBuilder.getLicenseByIdAndOrganizationId(BASEURL, LICENSE_ID, ORGANIZATION_ID)
+                        .andExpect(status().isNotFound());
+            }
+
+            @Test
+            @DisplayName("Should throws IllegalArgumentException which has a message error")
+            void shouldThrowsIllegalArgumentException() throws Exception {
+                requestBuilder.getLicenseByIdAndOrganizationId(BASEURL, LICENSE_ID, ORGANIZATION_ID)
+                        .andExpect(result -> assertTrue(result.getResolvedException() instanceof ResourceNotFoundException))
+                        .andExpect(result -> assertEquals("Unable to find license with License id",
+                                result.getResolvedException().getMessage()))
+                        .andDo(print());
+            }
         }
+        @Nested
+        @DisplayName("When the requested license is found")
+        class WhenRequestedLicenseIsFound{
+            private static final String DESCRIPTION = "description";
+            private static final String PRODUCT_NAME = "productName";
+            private static final String LICENSE_TYPE  = "licenseType";
+            private static final String COMMENT = "I AM DEFAULT";
 
-        @Test
-        @DisplayName("When licenseId or OrganizationId not found")
-        void getLicence(){
-
+            @BeforeEach
+            void returnFoundLicense() {
+                License license = new License(
+                        LICENSE_ID, DESCRIPTION, ORGANIZATION_ID,
+                        PRODUCT_NAME, LICENSE_TYPE, COMMENT);
+                when(licenseService.getLicense(LICENSE_ID, ORGANIZATION_ID)).thenReturn(license);
+            }
+            @Test
+            @DisplayName("Should return the correct information of the found license")
+            void getLicense() throws Exception {
+                requestBuilder.getLicenseByIdAndOrganizationId(BASEURL, LICENSE_ID, ORGANIZATION_ID)
+                        .andExpect(status().isOk())
+                        .andExpect(content().contentType("application/hal+json"))
+                        .andExpect(jsonPath("$.licenseId", is(LICENSE_ID)))
+                        .andExpect(jsonPath("$.organizationId", is(ORGANIZATION_ID)))
+                        .andDo(print());
+            }
         }
     }
 
